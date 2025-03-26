b3977657f20955b176e392f321d972b6c06c770f
DmitryMorozov <abdulaevabu@mail.ru>
2025-03-26 22:17:37 +0300

    [200~

---
+++ composables/useIntersectionObserver.js
@@ -0,0 +1,14 @@
+import { useIntersectionObserver } from '@vueuse/core'
+
+export const useIntersectionAnimation = (elementRef, animationCallback) => {
+  const { stop } = useIntersectionObserver(
+    elementRef,
+    ([{ isIntersecting }]) => {
+      if (isIntersecting && elementRef.value) {
+        animationCallback(elementRef.value)
+        stop()
+      }
+    },
+    { threshold: 0.1 }
+  )
+}
